cf65908739ff490f21485c183222532f7649c8c8
DNetORM <1184188167@qq.com>
2017-12-12 08:49:09 +0800

    sqlserver文件提交

---
+++ DNet.ORM.SqlServer/DbParameterFactory.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNet.DataAccess
+{
+    public class DbParameterFactory
+    {
+
+        /// <summary>
+        /// 创建DbParameter
+        /// </summary>
+        /// <returns></returns>
+        public static DbParameter CreateDbParameter(string parameterName, object value)
+        {
+            try
+            {
+
+                return new SqlParameter("@" + parameterName, value);
+
+            }
+            catch
+            {
+                throw new Exception("DbParameter错误！");
+            }
+        }
+
+        public static string GetParameterPrefix()
+        {
+            return "@";
+        }
+    }
+}
+
