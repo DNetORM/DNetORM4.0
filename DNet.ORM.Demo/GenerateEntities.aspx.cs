f10a4188e8cb00e78475c00487f33723937ab240
DNetORM <1184188167@qq.com>
2018-01-11 14:47:17 +0800

    生成实体文件demo

---
+++ DNet.ORM.Demo/GenerateEntities.aspx.cs
@@ -0,0 +1,28 @@
+﻿using DNet.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DNet.ORM.Demo
+{
+    public partial class GenerateEntities : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start(); //  开始监视代
+            using (DNetContext db = new DNetContext())
+            {
+                db.GenerateEntities("XXX.XXX.XXX");//files at project bin directory
+            }
+            stopwatch.Stop(); //  停止监视  
+            TimeSpan timespan = stopwatch.Elapsed; //  获取当前实例测量得出的总时间  
+            double milliseconds = timespan.TotalMilliseconds;  //  总毫秒数 
+            Response.Write("执行时间：" + milliseconds + "毫秒");
+        }
+    }
+}
